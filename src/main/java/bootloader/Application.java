6898897d419237d9af9e617596638620a90b8437
MalakaSenanayake <contact.malaka.senanayake@gmail.com>
2025-12-08 22:57:27 +0530

    Removed unwanted codes, images and etc

--- src/main/java/bootloader/Application.java
+++ src/main/java/bootloader/Application.java
@@ -10,8 +10,10 @@ import javafx.util.FxAnchorPaneUtil;
 import javafx.util.Configuration;
 import javafx.util.FxTheme;
 import utill.FxmlPath;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
@@ -37,28 +39,21 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) {
         initializeSystem();
-        if (true) { //check software status
-            try {
-                System.out.println(PropertyReader.BUSINESS_NAME);
-                AnchorPane rootPane = FxAnchorPaneUtil.setNew(FxmlPath.LOGIN);
-                Scene sc = new Scene(rootPane);
-                primaryStage.initStyle(StageStyle.DECORATED);
-                primaryStage.setResizable(false);
-                primaryStage.centerOnScreen();
-                primaryStage.getIcons().add(new Image("/image/appIcon.png"));
-                primaryStage.setScene(sc);
-                primaryStage.show();
-            } catch (Exception ex) {
-                Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        try {
+            System.out.println(PropertyReader.BUSINESS_NAME);
+            AnchorPane rootPane = FxAnchorPaneUtil.setNew(FxmlPath.PRINT_PAGE);
+            Scene sc = new Scene(rootPane);
+            primaryStage.initStyle(StageStyle.DECORATED);
+            primaryStage.setResizable(false);
+            primaryStage.centerOnScreen();
+            primaryStage.getIcons().add(new Image("/image/appIcon.png"));
+            primaryStage.setScene(sc);
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
 
-    //------------------------------------------------------------------------------------------------------------------
-//    private void loadJasper() {
-//
-//        JasperCompiler.compileReport(ReportPath.EXPIRE_DATE_LABEL);
-//    }
+    }
 
     //------------------------------------------------------------------------------------------------------------------
     private void initializeSystem() {
